be942bd9e597bd9e46f44e45959c18062ee13b4f
kenneth <kennethlindalen@gmail.com>
2020-09-02 13:01:37 +0200

    Variabel forandring

--- src/main/java/dev/kennethlindalen/FractalTree.java
+++ src/main/java/dev/kennethlindalen/FractalTree.java
@@ -30,7 +30,9 @@ class FractalTree extends JPanel {
         int x2 = x1 + (int) (Math.cos(Math.toRadians(grunnVinkel)) * stammeLengde * 4);
         int y2 = y1 + (int) (Math.sin(Math.toRadians(grunnVinkel)) * stammeLengde * 4);
 
-        if (Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1)) < 2) {
+        int avstandFraPunkter = (x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1);
+
+        if (Math.sqrt(avstandFraPunkter) < 2) {
             melding.setText("Gren er under 2px, blir derfor ikke tegnet.");
             return;
         }
@@ -49,7 +51,7 @@ class FractalTree extends JPanel {
         g.drawString(tilfeldig ? "" : "Vinkel:", 50, 370);
         g.drawString(tilfeldig ? "" : String.valueOf(vinkel), 95, 370);
         g.drawString("Stamme lengde: ", 50, 390);
-        g.drawString(skrivStammelengde ? Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1)) + "px" : "", 155, 390);
+        g.drawString(skrivStammelengde ? Math.sqrt(avstandFraPunkter) + "px" : "", 155, 390);
         g.drawString("Tilfeldig vinkel: ", 50, 410);
         g.drawString(tilfeldig ? "Ja" : "Nei", 150, 410);
 
