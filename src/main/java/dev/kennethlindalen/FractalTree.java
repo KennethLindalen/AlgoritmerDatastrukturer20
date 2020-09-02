c2f3c0a2c9fcdb7fdd5253c0884b015426352edc
kenneth <kennethlindalen@gmail.com>
2020-09-02 12:52:06 +0200

    Forandre stammelengde, forandre grunn vinkel, info display

--- src/main/java/dev/kennethlindalen/FractalTree.java
+++ src/main/java/dev/kennethlindalen/FractalTree.java
@@ -6,25 +6,29 @@ import java.awt.*;
 class FractalTree extends JPanel {
 
     private int dybde;
+    private int vinkel;
+    private int stammeLengde;
     private boolean tilfeldig;
     private JLabel melding = new JLabel();
 
-    public FractalTree(int dybde, boolean tilfeldig) {
+    public FractalTree(int dybde, boolean tilfeldig, int vinkel, int stammeLengde) {
         this.dybde = dybde;
         this.tilfeldig = tilfeldig;
+        this.vinkel = vinkel;
+        this.stammeLengde = stammeLengde;
     }
 
-    private void tegnTreet(Graphics g, int x1, int y1, double grunnVinkel, int dybde, boolean tilfeldig, int fontTykkelse) {
+    private void tegnTreet(Graphics g, int x1, int y1, double grunnVinkel, int dybde,
+                           boolean tilfeldig, int fontTykkelse, int stammeLengde, boolean skrivStammelengde) {
 
-        int vinkel = 20;
 
         if (dybde == 0) {
             return;
         }
 
         // Disse blir castet til int fordi Math.cos og Math.sin returnerer double
-        int x2 = x1 + (int) (Math.cos(Math.toRadians(grunnVinkel)) * dybde * 8);
-        int y2 = y1 + (int) (Math.sin(Math.toRadians(grunnVinkel)) * dybde * 8);
+        int x2 = x1 + (int) (Math.cos(Math.toRadians(grunnVinkel)) * stammeLengde * 4);
+        int y2 = y1 + (int) (Math.sin(Math.toRadians(grunnVinkel)) * stammeLengde * 4);
 
         if (Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1)) < 2) {
             melding.setText("Gren er under 2px, blir derfor ikke tegnet.");
@@ -39,8 +43,19 @@ class FractalTree extends JPanel {
         g2.setStroke(new BasicStroke(fontTykkelse));
         g2.drawLine(x1, y1, x2, y2);
 
-        tegnTreet(g, x2, y2, grunnVinkel - vinkel, dybde - 1, tilfeldig, fontTykkelse - 1);
-        tegnTreet(g, x2, y2, grunnVinkel + vinkel, dybde - 1, tilfeldig, fontTykkelse - 1);
+        g.drawString("Grener: ", 50, tilfeldig ? 370 : 350);
+        g.drawString(String.valueOf(this.dybde), 100, tilfeldig ? 370 : 350);
+        g.drawString(tilfeldig ? "" : "Vinkel:", 50, 370);
+        g.drawString(tilfeldig ? "" : String.valueOf(vinkel), 95, 370);
+        g.drawString("Stamme lengde: ", 50, 390);
+        g.drawString(skrivStammelengde ? Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1)) + "px" : "", 155, 390);
+        g.drawString("Tilfeldig vinkel: ", 50, 410);
+        g.drawString(tilfeldig ? "Ja" : "Nei", 150, 410);
+
+        tegnTreet(g, x2, y2, grunnVinkel - vinkel, dybde - 1, tilfeldig,
+                fontTykkelse - 1, (int) (stammeLengde * 0.9), false);
+        tegnTreet(g, x2, y2, grunnVinkel + vinkel, dybde - 1, tilfeldig,
+                fontTykkelse - 1, (int) (stammeLengde * 0.9), false);
     }
 
     public Dimension getPreferredSize() {
@@ -51,7 +66,7 @@ class FractalTree extends JPanel {
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
         g.setColor(Color.BLACK);
-        tegnTreet(g, 300, 375, -90, this.dybde, this.tilfeldig, 8);
+        tegnTreet(g, 300, 375, -90, this.dybde, this.tilfeldig, 8, stammeLengde, true);
         add(melding);
     }
 
--- src/main/java/dev/kennethlindalen/FractalTreeMain.java
+++ src/main/java/dev/kennethlindalen/FractalTreeMain.java
@@ -16,6 +16,11 @@ public class FractalTreeMain {
 
     public static void lagGUI() {
 
+        final int[] generasjonAntall = new int[1];
+        final boolean[] tilfeldigBool = {false};
+        final int[] vinkel = new int[1];
+        final int[] rotGrenLengde = new int[1];
+
         JFrame frame = new JFrame();
         frame.setTitle("Fraktal tre");
         frame.setSize(600, 800);
@@ -26,35 +31,79 @@ public class FractalTreeMain {
         mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.Y_AXIS));
 
         final JPanel treePanel = new JPanel();
-        treePanel.add(new FractalTree(9, false));
+        treePanel.add(new FractalTree(9, false, 20, 15));
 
         final JPanel optionsPanel = new JPanel(new GridBagLayout());
         GridBagConstraints gridBagConstraints = new GridBagConstraints();
 
-        JLabel treeGenerations = new JLabel();
-        treeGenerations.setText("Gren generasjoner: ");
+        generasjonAntall[0] = 9;
+        vinkel[0] = 25;
+        rotGrenLengde[0] = 15;
+
+        JLabel rotGrenLengdeLabel = new JLabel();
+        rotGrenLengdeLabel.setText("Lende på rot gren: ");
         gridBagConstraints.gridx = 0;
         gridBagConstraints.gridy = 0;
-        optionsPanel.add(treeGenerations, gridBagConstraints);
+        optionsPanel.add(rotGrenLengdeLabel, gridBagConstraints);
 
-        final int[] generasjonAntall = new int[1];
-        final boolean[] tilfeldigBool = {false};
 
-        final JSlider treGenerasjonSlider = new JSlider(JSlider.HORIZONTAL, 1, 8, 1);
-        treGenerasjonSlider.addChangeListener(new ChangeListener() {
+        final JSlider rotGrenLengdeSlider = new JSlider(JSlider.HORIZONTAL, 10, 25, 15);
+        rotGrenLengdeSlider.addChangeListener(new ChangeListener() {
             @Override
             public void stateChanged(ChangeEvent e) {
-                generasjonAntall[0] = treGenerasjonSlider.getValue();
+                rotGrenLengde[0] = rotGrenLengdeSlider.getValue();
                 treePanel.removeAll();
-                treePanel.add(new FractalTree(generasjonAntall[0], tilfeldigBool[0]));
+                treePanel.add(new FractalTree(generasjonAntall[0], tilfeldigBool[0], vinkel[0], rotGrenLengde[0]));
                 treePanel.revalidate();
 
             }
         });
         gridBagConstraints.gridx = 1;
         gridBagConstraints.gridy = 0;
+        optionsPanel.add(rotGrenLengdeSlider, gridBagConstraints);
+
+        JLabel treeGenerations = new JLabel();
+        treeGenerations.setText("Gren generasjoner: ");
+        gridBagConstraints.gridx = 0;
+        gridBagConstraints.gridy = 1;
+        optionsPanel.add(treeGenerations, gridBagConstraints);
+
+        final JSlider treGenerasjonSlider = new JSlider(JSlider.HORIZONTAL, 1, 10, 9);
+        treGenerasjonSlider.addChangeListener(new ChangeListener() {
+            @Override
+            public void stateChanged(ChangeEvent e) {
+                generasjonAntall[0] = treGenerasjonSlider.getValue();
+                treePanel.removeAll();
+                treePanel.add(new FractalTree(generasjonAntall[0], tilfeldigBool[0], vinkel[0], rotGrenLengde[0]));
+                treePanel.revalidate();
+
+            }
+        });
+        gridBagConstraints.gridx = 1;
+        gridBagConstraints.gridy = 1;
         optionsPanel.add(treGenerasjonSlider, gridBagConstraints);
 
+        JLabel bestemVinkel = new JLabel();
+        bestemVinkel.setText("Grunnvinkel for gren: ");
+        gridBagConstraints.gridx = 0;
+        gridBagConstraints.gridy = 2;
+        optionsPanel.add(bestemVinkel, gridBagConstraints);
+
+
+        final JSlider vinkelSlider = new JSlider(JSlider.HORIZONTAL, 1, 90, 25);
+        vinkelSlider.addChangeListener(new ChangeListener() {
+            @Override
+            public void stateChanged(ChangeEvent e) {
+                vinkel[0] = vinkelSlider.getValue();
+                treePanel.removeAll();
+                treePanel.add(new FractalTree(generasjonAntall[0], tilfeldigBool[0], vinkel[0], rotGrenLengde[0]));
+                treePanel.revalidate();
+
+            }
+        });
+        gridBagConstraints.gridx = 1;
+        gridBagConstraints.gridy = 2;
+        optionsPanel.add(vinkelSlider, gridBagConstraints);
 
         JButton tilfeldigVinkelKnapp = new JButton();
         tilfeldigVinkelKnapp.setText("Tilfeldige Vinkler");
@@ -63,12 +112,12 @@ public class FractalTreeMain {
             public void actionPerformed(ActionEvent e) {
                 tilfeldigBool[0] = !tilfeldigBool[0];
                 treePanel.removeAll();
-                treePanel.add(new FractalTree(generasjonAntall[0], tilfeldigBool[0]));
+                treePanel.add(new FractalTree(generasjonAntall[0], tilfeldigBool[0], vinkel[0], rotGrenLengde[0]));
                 treePanel.revalidate();
             }
         });
         gridBagConstraints.gridx = 0;
-        gridBagConstraints.gridy = 1;
+        gridBagConstraints.gridy = 3;
         optionsPanel.add(tilfeldigVinkelKnapp, gridBagConstraints);
 
         mainPanel.add(treePanel);
